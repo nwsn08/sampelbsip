08acce02cb7447d5cbbe8eaa8c68f43f59a95b92
sugengekosaputro <sugengekosaputro96@gmail.com>
2023-12-14 13:41:06 +0700

    remove yaml

---
+++ layouts/SingleLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app style="position: relative; height: 100vh">
+    <div class="background-image"></div>
+    <Navigation/>
+    <v-main style="position: relative; z-index: 1;">
+      <v-container>
+        <div>
+          <Header />
+        </div>
+      </v-container>
+      <slot/>
+
+    </v-main>
+    <div class="text-center d-flex flex-column bg-teal-darken-3 pa-4" style="height: 100px">
+      <p class="align-end">
+        Jl. Pahlawan No. 2, Grati, Kabupaten Pasuruan, Jawa Timur 67184
+      </p>
+      <v-divider></v-divider>
+
+      <div class="align-end">
+        {{ new Date().getFullYear() }} — <strong>LPSI</strong>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script setup>
+const icons = [
+  'mdi-facebook',
+  'mdi-twitter',
+  'mdi-linkedin',
+  'mdi-instagram',
+]
+</script>
+
+<script>
+export default {
+  data: () => ({
+    step: 1,
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/images/bg-header.jpg'); /* Replace 'your-background-image.jpg' with your actual image URL */
+  background-size: cover;
+  opacity: 0.1; /* Adjust the opacity value as needed */
+  z-index: 0;
+}
+</style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Welcome to the Dashboard</h2>
+    <div class="d-flex justify-space-between mt-10">
+      <DashboardMainCard title="Menunggu Diproses" value="100"/>
+      <DashboardMainCard title="Sedang Diproses" value="100"/>
+      <DashboardMainCard title="Selesai" value="100"/>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
